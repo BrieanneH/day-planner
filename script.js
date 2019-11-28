3e85f2a39f4fa0850afdad2e8ad4ab54d67c5c5d
BrieanneH <howard.brieanne@gmail.com>
2019-11-27 21:17:44 -0600

    creating loop and rows

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $('#currentDay').text(moment().format("MM DoYY"));
+    $('#currentDay').text(moment().format("MM Do YY"));
+    //creating my loop
+    for (let i = 0; i < 8; i++) {
+
+        var row = $('<div class = "row">');
+
+        var col1= $('viv class = "col-med-2"><p class ="hour">';)
+    }
+
+
 })
+
