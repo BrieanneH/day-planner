4a506fe303ecdc20eb408de934eb6e93c90601a1
BrieanneH <howard.brieanne@gmail.com>
2019-11-27 22:30:47 -0600

    making mutliple var show

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+  
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
+
+
+
 $(document).ready(function(){
-    $('#currentDay').text(moment().format("MM Do YY"));
+    $('#currentDay').text(moment().format("dddd, MMMM Do"));
     //creating my loop
-    for (let i = 0; 9 < 18; i++) {
+    for (let i = 9; i < 18; i++) {
+        //create row
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+        //create col
+        var col1 = $('<div class="col-sm-2"> <p class="hour">'+ convertAMPM(i) + '</p>');
 
-        var row = $('<div class = "row">');
-
-        var col1= $('div class = "col-med-2"><p class ="hour">'+ convertAMPM(i) + '</p>');
-
-        var caol2 = $('div class = "col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`)');
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
         row.append(col1);
         row.append(col2);
         row.append(col3);
+        //append rows to container
         $(".container").append(row);
-     };
+
+        getLocalStorage(i);
+     }
 
      function convertAMPM(hours) {
         var ampm = hours >= 12 ? 'pm' : 'am';
@@ -44,19 +57,10 @@ setInterval(function() {
 }, 1000);
 
 
-var saveBtn = $(".saveBtn");
-saveBtn.on('click',function(){
-    let eventID=$(this).attr('id');
-    let eventText=$(this).parent().siblings().children('.description').val();
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function(){
+    let eventId = $(this).attr('id');
+    let eventText = $(this).parent().siblings().children('.description').val();
     localStorage.setItem(eventId, eventText);
-
-    function getLocalStorage(){
-        let value =loclStorage.getItem();
-        if (value){
-            $('#text').text(value);
-        }
-    }
-}
-
 });});
 
