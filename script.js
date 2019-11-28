8df4194def612e417a1d005e69862aa950aa6056
BrieanneH <howard.brieanne@gmail.com>
2019-11-27 21:40:54 -0600

    formatting AMPM

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
         var row = $('<div class = "row">');
 
-        var col1= $('div class = "col-med-2"><p class ="hour">'+ AMPM(i) + '</p>');
+        var col1= $('div class = "col-med-2"><p class ="hour">'+ convertAMPM(i) + '</p>');
 
         var caol2 = $('div class = "col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`)');
         var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
@@ -13,8 +13,15 @@ $(document).ready(function(){
         row.append(col2);
         row.append(col3);
         $(".container").append(row);
+     };
 
+     function convertAMPM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
     }
+convertAMPM();
 
 
 })
